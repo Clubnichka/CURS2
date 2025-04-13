89396c525aa3142f5410db9bc14f06b83730b6a3
Lebedev A.D. <proventusavenici@gmail.com>
2025-04-13 17:37:35 +0300

    Добавьте файлы проекта.

---
+++ CURS2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace KitchenSceneTao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.AccumBits = ((byte)(0));
+            this.glControl.AutoCheckErrors = false;
+            this.glControl.AutoFinish = false;
+            this.glControl.AutoMakeCurrent = true;
+            this.glControl.AutoSwapBuffers = true;
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.ColorBits = ((byte)(32));
+            this.glControl.DepthBits = ((byte)(16));
+            this.glControl.Location = new System.Drawing.Point(12, 12);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(507, 426);
+            this.glControl.StencilBits = ((byte)(0));
+            this.glControl.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.glControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SimpleOpenGlControl glControl;
+    }
+}
+
